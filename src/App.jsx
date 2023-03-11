5745d1f8ef5ce6ab5fb1b3388e05c36de0950a66
Sherklan12e <davismendoza.08@gmail.com>
2023-03-11 11:15:15 -0300

    3D whith react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import Who from "./components/Who"
+import Contact from "./components/Contact"
+import styled from 'styled-components'
+import Hero from "./components/Hero"
+import Works from "./components/Works"
+
+const Container = styled.div`
+ 
+  height:100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: initial;
+  color: white;
+  background: url("./img/bg.jpg");  
+
+`
+
+
 function App() {
 
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Hero/>
+      <Contact/>
+      <Works/>
+      <Who/>
+    </Container>
   )
 }
 
